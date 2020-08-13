20728dea36d7ae9040d4aef07745932d714062e8
lzy <zhaoyang.lu@nldou.com>
2020-08-13 21:19:23 +0800

    增加模拟登陆功能

--- src/Config/xiaoe.php
+++ src/Config/xiaoe.php
@@ -11,5 +11,7 @@ return [
     'secret_key' => env('XIAOE_SECRET_KEY', ''),
     'token' => env('XIAOE_TOKEN', ''),
     'aes_key' => env('XIAOE_AES_KEY', ''),
-    'refund_notify_url' => env('XIAOE_REFUND_NOTIFY_URL', '')
+    'refund_notify_url' => env('XIAOE_REFUND_NOTIFY_URL', ''),
+    'username' => env('XIAOE_LOGIN_USERNAME', ''),
+    'password' => env('XIAOE_LOGIN_PASSWORD', '')
 ];
